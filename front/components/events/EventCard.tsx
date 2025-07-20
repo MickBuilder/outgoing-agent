f951210afe0a784f2f7225df884d75f1b06127db
Mike <bmikdaam@gmail.com>
2025-07-20 11:58:49 +0200

    to compile

--- front/components/events/EventCard.tsx
+++ front/components/events/EventCard.tsx
@@ -1,4 +1,4 @@
-import { Card, CardHeader, CardTitle, CardDescription, CardContent, CardFooter } from "@/components/ui/card";
+import { Card, CardHeader, CardTitle, CardContent, CardFooter } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { Event } from "@/lib/hooks/use-chat";
 
